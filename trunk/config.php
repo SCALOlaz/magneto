a5cbc647b9e60adc7a7f4dbc8b0696c2db12969b
scalolaz_scat@mail.ru <scalolaz_scat@mail.ru@28eafe48-1fa4-ebfb-c054-3a7c3e4059c6>
2012-02-12 16:57:05 +0000

    2 Update

---
+++ trunk/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = '';
+$dbhost = '';
+$dbport = '';
+$dbname = '';
+$dbuser = '';
+$dbpasswd = '';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', false);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
