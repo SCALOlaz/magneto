b8cf01f1bc14023460e4e49feec7ba3db5b6d25f
scalolaz_scat@mail.ru <scalolaz_scat@mail.ru@28eafe48-1fa4-ebfb-c054-3a7c3e4059c6>
2012-02-12 17:05:58 +0000

    3 Update

---
+++ includes/acp/info/acp_newspage.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+*
+* @package - NV newspage
+* @version $Id$
+* @copyright (c) nickvergessen ( http://www.flying-bits.org/ )
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package module_install
+*/
+class acp_newspage_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_newspage',
+			'title'		=> 'NEWS',
+			'version'	=> '1.0.5.1',
+			'modes'		=> array(
+				'adjust_news'	=> array(
+					'title'		=> 'NEWS_CONFIG',
+					'auth'		=> 'acl_a_board',
+					'cat'		=> array('ACP_BOARD_CONFIGURATION'),
+				),
+			),
+		);
+	}
+}
+
+?>
